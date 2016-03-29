d3dbf257fc7662a68d12719558d2935a92b39be3
James Watson <siidney.watson@gmail.com>
2016-03-29 10:04:42 -0600

    Completed Chapter 4

---
+++ Chapter04/exercises/4.35/ex_435.cpp
@@ -0,0 +1,35 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  ex_435.cpp
+ *
+ *    Description:  Exercise 4.35 - Enforcing Privacy with Cryptography
+ *
+ *        Version:  1.0
+ *        Created:  29/03/16 08:17:45
+ *       Revision:  none
+ *       Compiler:  g++
+ *
+ *         Author:  Siidney Watson - siidney.watson@gmail.com
+ *   Organization:  LolaDog Studio
+ *
+ * =====================================================================================
+ */
+#include "EncryptionSystem.h"
+
+int main(int argc, const char *argv[]){
+    EncryptionSystem es;
+
+    int code = 0;
+
+    std::cout << "Welcome to encryption/decryption system\n" << std::endl;
+    std::cout << "Enter a 4 digit pass code to be encrypted: ";
+    std::cin >> code;
+
+    es.encrypt(code);
+
+    std::cout << "\nEncrypted pass code: " << es.getEncPass() << "\n" << std::endl;
+
+    std::cout << "Decrypted pass code: " << es.decrypt() << std::endl;
+    return 0;
+}
