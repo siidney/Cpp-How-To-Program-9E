434cff3dde6af38e2a3b59308139aff6463298e3
Siidney Watson <siidney.watson.work@gmail.com>
2016-08-17 19:58:52 -0600

    Completed 13.13

---
+++ Chapter13/exercises/13.13/Square.h
@@ -0,0 +1,31 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  Square.h
+ *
+ *    Description:  Exercise 13.13 - Shape Hierarchy
+ *                  2D Square
+ *
+ *        Version:  1.0
+ *        Created:  17/08/16 15:11:09
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Siidney Watson - siidney.watson.work@gmail.com
+ *   Organization:  LolaDog Studio
+ *
+ * =====================================================================================
+ */
+#pragma once
+
+#include "TwoDimensionalShape.h"
+
+class Square : public TwoDimensionalShape{
+    public:
+        Square(double = 0.0f, double = 0.0f);
+
+        virtual double getArea() const;
+        virtual void print() const;
+
+    private:
+};
---
+++ Chapter13/exercises/13.13/ex_1313.cpp
@@ -0,0 +1,78 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  ex_1313.cpp
+ *
+ *    Description:  Exercise 13.13 - Shape Hierarchy
+ *
+ *        Version:  1.0
+ *        Created:  17/08/16 15:09:52
+ *       Revision:  none
+ *       Compiler:  g++
+ *
+ *         Author:  Siidney Watson - siidney.watson.work@gmail.com
+ *   Organization:  LolaDog Studio
+ *
+ * =====================================================================================
+ */
+#include "Shape.h"
+#include "TwoDimensionalShape.h"
+#include "ThreeDimensionalShape.h"
+#include "Square.h"
+#include "Rectangle.h"
+#include "Circle.h"
+#include "Triangle.h"
+#include "Cube.h"
+#include "Cuboid.h"
+#include "Sphere.h"
+
+#include <iostream>
+#include <vector>
+#include <typeinfo>
+
+int main(int argc, const char *argv[]){
+    // 2D shapes
+    Square square(2);
+    Rectangle rectangle(3, 6);
+    Circle circle(10);
+    Triangle triangle(15, 4);
+
+    // 3D shapes
+    Cube cube(5);
+    Cuboid cuboid(5, 4, 10);
+    Sphere sphere(10);
+
+    std::vector<Shape*>shapes(7);
+
+    shapes[0] = &square;
+    shapes[1] = &rectangle;
+    shapes[2] = &circle;
+    shapes[3] = &triangle;
+    shapes[4] = &cube;
+    shapes[5] = &cuboid;
+    shapes[6] = &sphere;
+
+    for(size_t i=0; i<shapes.size(); ++i){
+        shapes[i]->print();
+
+        // downcast pointer to TwoDimensionalShape
+        TwoDimensionalShape *twoDimDerivedPtr = dynamic_cast<TwoDimensionalShape*>(shapes[i]);
+
+        if(twoDimDerivedPtr != 0){ // 0 if not TwoDimensionalShape
+            std::cout << "\nArea: " << twoDimDerivedPtr->getArea() << std::endl;
+        }else{
+            // three dimensional shape print volume
+
+            // downcast pointer to ThreeDimensionalShape
+            ThreeDimensionalShape *threeDimDerivedPtr = dynamic_cast<ThreeDimensionalShape*>(shapes[i]);
+            if(threeDimDerivedPtr != 0){    // 0 if not ThreeDimensionalShape
+                std::cout << "\nSurface Area: " << threeDimDerivedPtr->getArea()
+                          << "\nVolume: " << threeDimDerivedPtr->getVolume() << std::endl;
+            }
+        }
+
+        std::cout << "==========" << std::endl;
+    }
+
+    return 0;
+}
