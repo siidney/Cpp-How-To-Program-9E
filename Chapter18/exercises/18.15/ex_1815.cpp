be2bdd97820657892e20583fa99a910c2a9cda86
Siidney Watson <siidney.watson.work@gmail.com>
2017-02-17 14:45:48 -0600

    Chapter 18 Update

---
+++ Chapter18/exercises/18.15/ex_1815.cpp
@@ -0,0 +1,26 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  ex_1815.cpp
+ *
+ *    Description:  Exercise 18.15 - Solving Cryptograms
+ *
+ *        Version:  1.0
+ *        Created:  16/02/17 15:03:59
+ *       Revision:  none
+ *       Compiler:  g++
+ *
+ *         Author:  Siidney Watson - siidney.watson.work@gmail.com
+ *   Organization:  LolaDog Studio
+ *
+ * =====================================================================================
+ */
+#include "Cryptogram.h"
+
+int main(int argc, const char* argv[]){
+    Cryptogram crypt;
+
+    crypt.run();
+
+    return 0;
+}
