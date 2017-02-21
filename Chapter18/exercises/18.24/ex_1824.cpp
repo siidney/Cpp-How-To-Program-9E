9c0b6e3e15a8dab227bc2d3fa65f03bfbed6bdda
Siidney Watson <siidney.watson.work@gmail.com>
2017-02-20 18:28:17 -0600

    Chapter 18 progress

---
+++ Chapter18/exercises/18.24/ex_1824.cpp
@@ -0,0 +1,35 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  ex_1824.cpp
+ *
+ *    Description:  Exercise 18.24 - Letter Pyramid
+ *
+ *        Version:  1.0
+ *        Created:  20/02/17 18:07:49
+ *       Revision:  none
+ *       Compiler:  g++
+ *
+ *         Author:  Siidney Watson - siidney.watson.work@gmail.com
+ *   Organization:  LolaDog Studio
+ *
+ * =====================================================================================
+ */
+#include <iostream>
+#include <string>
+
+int main(int argc, const char* argv[]){
+    std::string base = "abcdefghijklmnopqrstuvwxyz";
+
+    for(size_t i=0, j=1; i<14; ++i, j+=2){
+        // print spaces
+        std::cout << std::string(14 - i, ' ');
+
+        for(size_t k=0; k<j; ++k){
+            std::cout << base[i + k];
+        }
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
