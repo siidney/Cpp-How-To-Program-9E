68d99991ea574ad852eb754c73a9495b1f52c989
Siidney Watson <siidney.watson.work@gmail.com>
2018-02-20 12:45:32 -0600

    Added placeholder for empty folder for git tracking

---
+++ Chapter20/exercises/20.24/ex_2024.cpp
@@ -0,0 +1,32 @@
+/*
+ * =============================================================================
+ *
+ *       Filename:  ex_2024.cpp
+ *
+ *    Description:  Exercise 20.24 - Level-Order Binary Tree Traversal.
+ *
+ *        Version:  1.0
+ *        Created:  19/02/18 15:37:49
+ *       Revision:  none
+ *       Compiler:  g++
+ *
+ *         Author:  Siidney Watson - siidney.watson.work@gmail.com
+ *   Organization:  LolaDog Studio
+ *
+ * =============================================================================
+ */
+#include <iostream>
+
+#include "Tree.hpp"
+
+/**
+ * This is the main method.
+ * @param argc.
+ * @param argv[].
+ * @return int.
+ */
+int main(int argc, char* argv[]) {
+    Tree<int> intTree;
+
+    return 0;
+}  // end method main
