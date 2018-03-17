d41085b293262199094924804b270e06edc543e7
Siidney Watson <siidney.watson.work@gmail.com>
2018-03-17 10:03:21 -0600

    Chapter 21 update

---
+++ Chapter21/exercises/21.29/custsearch.hpp
@@ -0,0 +1,84 @@
+/*
+ * =============================================================================
+ *
+ *       Filename:  custsearch.hpp
+ *
+ *    Description:  Exercise 21.29 - Write Your Own String Searching Functions.
+ *
+ *        Version:  1.0
+ *        Created:  17/03/18 09:38:26
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Siidney Watson - siidney.watson.work@gmail.com
+ *   Organization:  LolaDog Studio
+ *
+ * =============================================================================
+ */
+#pragma once
+
+/**
+ * Locates the first occurance of char c in string s.
+ * A pointer to c in s is return if found.
+ * @param const char*
+ * @param int
+ * @return char*
+ */
+char* strchr(const char* s, int c) {
+    
+}  // end method strchr
+
+/**
+ * Searches from the end of string s locating the last occurance of char c.
+ * A pointer to c in s is return if found.
+ * @param const char*
+ * @param int
+ * @return char*
+ */
+char* strrchr(const char* s, int c) {
+    
+}  // end method strrchr
+
+/**
+ * Determines and returns the length of the initial segment of string s1
+ * consisting only of characters contained in string s2.
+ * @param const char*
+ * @param const char*
+ * @return size_t
+ */
+size_t strspn(const char* s1, const char* s2) {
+    
+}  // end method strspn
+
+/**
+ * Locates the first occurance in string s1 of any char in string s2.
+ * A pointer to the char is returned if found.
+ * @param const char*
+ * @param const char*
+ * @return char*
+ */
+char* strpbrk(const char* s1, const char* s2) {
+    
+}  // end method strpbrk
+
+/**
+ * Determines and returns the length of the initial segment of string s1
+ * consisting of characters not contained in string s2.
+ * @param const char*
+ * @param const char*
+ * @return char*
+ */
+char* strpbrk(const char* s1, const char* s2) {
+    
+}  // end method strpbrk
+
+/**
+ * Locates the first occurance in string s1 of string s2.
+ * Returns a pointer to the string in s1 if found.
+ * @param const char*
+ * @param const char*
+ * @return char*
+ */
+char* strstr(const char* s1, const char* s2) {
+    
+}  // end method strstr
---
+++ Chapter21/exercises/21.29/ex_2129.cpp
@@ -0,0 +1,32 @@
+/*
+ * =============================================================================
+ *
+ *       Filename:  ex_2129.cpp
+ *
+ *    Description:  EXercise 21.29 - Write Your Own String Searching Functions.
+ *
+ *        Version:  1.0
+ *        Created:  17/03/18 09:37:31
+ *       Revision:  none
+ *       Compiler:  g++
+ *
+ *         Author:  Siidney Watson - siidney.watson.work@gmail.com
+ *   Organization:  LolaDog Studio
+ *
+ * =============================================================================
+ */
+#include <iostream>
+
+#include "custsearch.hpp"
+
+/**
+ * This is the main method.
+ * @param argc.
+ * @param argv[].
+ * @return int.
+ */
+int main(int argc, char* argv[]) {
+    
+
+    return 0;
+}  // end method main
