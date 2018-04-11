d1597ef984c388eecbb950de55094e69fe4cbfa0
Siidney Watson <siidney.watson.work@gmail.com>
2018-04-11 13:04:29 -0600

    Chapter 21 Updates

---
+++ Chapter21/exercises/21.40/.custcopycatptr.cpp@neomake_759_160.cpp
@@ -0,0 +1,18 @@
+/*
+ * =============================================================================
+ *
+ *       Filename:  custcopycatptr.cpp
+ *
+ *    Description:   
+ *
+ *        Version:  1.0
+ *        Created:  10/04/18 11:25:09
+ *       Revision:  none
+ *       Compiler:  g++
+ *
+ *         Author:  Siidney Watson - siidney.watson.work@gmail.com
+ *   Organization:  LolaDog Studio
+ *
+ * =============================================================================
+ */
+
